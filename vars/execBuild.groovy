477229e6f3a263a96060dc80bb14bd1547faca37
fabe273394 <famidha.begum@elektrobit.com>
2022-06-16 16:09:43 +0530

    Added gradle build pipeline

---
+++ vars/execBuild.groovy
@@ -0,0 +1,28 @@
+def call(String command) {
+	if (command == "gradle") {
+		pipeline {
+ 		   agent {
+			 node "master" 
+		  }
+    		   stages {
+        		stage ("Checkout") {
+            			steps {
+                			script {
+                    				cleanWs()
+                    				checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/FamidhaThurab/GradleProject.git']]])
+               				 }
+           			 }
+       			 }
+        		stage ("Build") {
+            			steps {
+                			script {
+                   				 sh label: '', script: '''#!/bin/bash -ex
+                       					 ./gradlew build
+                       					 ./gradlew run
+                   				 '''
+               				 }
+           			 }
+       			 }
+   		 }
+	}	
+}
