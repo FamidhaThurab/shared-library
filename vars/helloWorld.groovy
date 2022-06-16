e2494fab7ee1e97b5a0e2698bcef23cd247f77f0
fabe273394 <famidha.begum@elektrobit.com>
2022-06-16 12:42:08 +0530

    Added maps to take arguments

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
- def call(String name, String dayOfWeek) {
-	sh "echo Hello ${name}."
-	sh "echo Today is ${dayOfWeek}."
+ def call(Map config = [:]) {
+	sh "echo Hello ${config.name}."
+	sh "echo Today is ${config.dayOfWeek}."
 }
