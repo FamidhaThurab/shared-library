f64e0b596f54c5a97b56c0922e4a0c666f2916e0
FamidhaThurab <107538020+FamidhaThurab@users.noreply.github.com>
2022-06-16 16:29:30 +0530

    Update execBuild.groovy

--- vars/execBuild.groovy
+++ vars/execBuild.groovy
@@ -1,29 +1,30 @@
 def call(String command) {
-	if (command == "gradle") {
-		pipeline {
- 		   agent {
-			 node "master" 
-		  }
-    		   stages {
-        		stage ("Checkout") {
-            			steps {
-                			script {
-                    				cleanWs()
-                    				checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/FamidhaThurab/GradleProject.git']]])
-               				 }
-           			 }
-       			 }
-        		stage ("Build") {
-            			steps {
-                			script {
-                   				 sh label: '', script: '''#!/bin/bash -ex
-                       					 ./gradlew build
-                       					 ./gradlew run
-                   				 '''
-               				 }
-           			 }
-       			 }
-   		 }
-	}	
-}
+    if (command == "gradle") {
+        pipeline {
+            agent { 
+                node "master" 
+            }
+            stages {
+                stage ("Checkout") {
+                    steps {
+                        script {
+                            cleanWs()
+                            checkout([$class: 'GitSCM', branches: [[name: '*/main']], 
+				userRemoteConfigs: [[url: 'https://github.com/FamidhaThurab/GradleProject.git']]])
+                        }
+                    }
+                }
+                stage ("Build") {
+                    steps {
+                        script {
+                            sh label: '', script: '''#!/bin/bash -ex
+                        ./gradlew build
+                        ./gradlew run
+                    '''
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
